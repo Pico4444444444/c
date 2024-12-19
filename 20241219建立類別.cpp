d5c1ea7603529c28c45e8577cd99a0ccd224f7c4
Pico4444444444 <313044@s.lhvs.tyc.edu.tw>
2024-12-19 15:23:48 +0800

    Create 20241219建立類別.cpp

---
+++ 20241219建立類別.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class myTime{
+    public:
+        int hour;
+        int minutes;
+        void printTime();
+};
+
+void myTime::printTime(){
+    cout<<hour<<":"<<minutes<<"\n";
+}
+int main(){
+    myTime now, open;
+    now.hour = 8;
+    now.minutes = 18;
+    cout<<"現在時間:";
+    now.printTime();
+    open.hour = 9;
+    open.minutes = 10;
+    cout<<"開門時間:";
+    open.printTime();
+    
+    
+}
